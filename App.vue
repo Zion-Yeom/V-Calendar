20bfbb2721d3e103ab962b3cadba26568db50021
Zion-Yeom <100013558+Zion-Yeom@users.noreply.github.com>
2022-04-01 15:29:30 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <!--    <vue-calendar></vue-calendar>-->
+    <calendar-j></calendar-j>
+  </div>
+</template>
+
+<script>
+import CalendarJ from "@/components/CalendarJ";
+export default {
+  name: 'App',
+  components: {
+    CalendarJ,
+  },
+  data(){
+    return {
+
+    }
+  },
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: 'Nanum Gothic', sans-serif;
+  box-sizing: border-box;
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+
+
+
+</style>
